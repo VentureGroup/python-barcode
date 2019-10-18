0a1feb8a7b9ef41e48d84c2c0f6391b8e25c702b
yfedyk <yuriy.fedyk@probegin.com>
2019-10-18 18:05:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@
 import sys
 from os.path import join, dirname
 
-import barcode as pkg
-
 from setuptools import setup, find_packages
-
 
 # Avoid name clashes if the user has Python 2 and 3 installed
 console_script = 'pybarcode{0}'.format(sys.version_info[0])
@@ -21,16 +18,26 @@ with open(join(dirname(__file__), 'README.rst')) as fp:
 
 
 setup(
-    name=pkg.__project__,
-    version=pkg.__release__,
+    name='python-barcode',
+    version='0.0.2',
     packages=find_packages(),
-    url=pkg.__url__,
-    license=pkg.__license__,
-    author=pkg.__author__,
-    author_email=pkg.__author_email__,
-    description=pkg.__description__,
+    url='https://github.com/VentureGroup/python-barcode',
+    license='MIT',
+    author='Vadain Development',
+    author_email='',
+    description=('Create standard barcodes with Python. No external '
+                 'modules needed (optional PIL support included).'),
     long_description=long_desc,
-    classifiers=pkg.__classifiers__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Graphics',
+    ],
     entry_points={
         'console_scripts':
             ['{0} = barcode.pybarcode:main'.format(console_script)],
